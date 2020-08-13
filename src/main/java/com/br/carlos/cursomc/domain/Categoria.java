bc4b16154f4ff74156e4758f284b8e2c819ceaa2
Carlos Eduardo Pereira <cadu.carlos514@gmail.com>
2020-08-13 19:27:48 -0300

    banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/br/carlos/cursomc/domain/Categoria.java
+++ src/main/java/com/br/carlos/cursomc/domain/Categoria.java
@@ -2,6 +2,12 @@ package com.br.carlos.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -20,6 +26,8 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Integer getId() {
 		return id;
 	}
