7852c41ea436db9e21c7acd814949a15c5b7e392
Carlos Eduardo Pereira <cadu.carlos514@gmail.com>
2020-08-14 15:12:41 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/br/carlos/cursomc/domain/Produto.java
+++ src/main/java/com/br/carlos/cursomc/domain/Produto.java
@@ -8,29 +8,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable{
+public class Produto implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	
-	private Integer  id;
+	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	private List<Produto> produtos = new ArrayList<>();
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	public Categoria() {
+	public Produto() {
 		
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Integer getId() {
@@ -48,16 +51,25 @@ public class Categoria implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	@ManyToMany(mappedBy = "categorias")
-	public List<Produto> getProdutos() {
-		return produtos;
+
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 	
+	@ManyToMany
+	@JoinTable(name="PRODUTO_CATEGORIA", joinColumns = 	@JoinColumn(name="produto_id"),
+	inverseJoinColumns = @JoinColumn(name="categoria_id"))
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
 
 	@Override
 	public int hashCode() {
@@ -75,7 +87,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -83,7 +95,10 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
+	
+	 	
 
+	
 	
 	
 
---
+++ src/main/java/com/br/carlos/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.br.carlos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.br.carlos.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+
+}
